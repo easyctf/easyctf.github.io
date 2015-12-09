76be3bf2a11b3386cf07230aa80d046602b37564
Michael Zhang <failed.down@gmail.com>
2015-12-09 09:49:50 +0000

    stuff

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-function setSplashHeight() {
+function setSize() {
 	if (window.innerWidth <= 767) {
 		$("#splash").css("height", window.innerHeight);
 	} else {
 		$("#splash").css("height", Math.max(500, window.innerHeight - 90));
 	}
+	$("#navbar-filler").css("height", $("#nav").outerHeight());
 }
 
 $(document).ready(function() {
-	setSplashHeight();
-	
+	setSize();
+	$("[data-toggle=\"tooltip\"]").tooltip();
 	smoothScroll.init({
-		speed: 240,
-		easing: 'easeOutCubic',
+		speed: 480,
+		easing: "easeOutCubic",
+		offset: $("#nav").outerHeight(),
 		updateURL: false
 	});
 });
 
 $(document).resize(function() {
-	setSplashHeight();
+	setSize();
+});
+
+$(document).scroll(function() {
+	var scroll = $(window).scrollTop();
+	if ($("#navbar-filler").is(":visible") ? (scroll > $("#navbar-filler").position().top) : (scroll > $("#nav").position().top)) {
+		$("#nav").addClass("navbar-fixed-top");
+		$("#navbar-filler").show();
+	} else {
+		$("#nav").removeClass("navbar-fixed-top");
+		$("#navbar-filler").hide();
+	}
 });
